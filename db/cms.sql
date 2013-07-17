bf5b4b2a7acbdf178bd704f5ff28a8d0a291f1d2
KunalGautam <kunal@ikunal.in>
2013-07-17 17:32:24 +0530

    SQL Changes
    renamed table, using spaces in table name is not a general practice
    made userid primary key, username and email unique key, collation to
    utf8_bin and Varchar(255)
    
    made id primary, name unique, name and content as Text and collation to
    utf8_bin

--- db/cms.sql
+++ db/cms.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 3.2.4
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 17, 2013 at 07:00 AM
--- Server version: 5.5.24-log
--- PHP Version: 5.3.13
+-- Generation Time: Jul 17, 2013 at 05:30 PM
+-- Server version: 5.1.41
+-- PHP Version: 5.3.1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -23,34 +22,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `data table`
---
--- Creation: Jul 16, 2013 at 08:48 PM
+-- Table structure for table `data`
 --
 
-CREATE TABLE IF NOT EXISTS `data table` (
+CREATE TABLE IF NOT EXISTS `data` (
   `id` tinyint(4) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) DEFAULT NULL,
-  `content` varchar(10000) DEFAULT NULL,
+  `name` tinytext CHARACTER SET utf8 COLLATE utf8_bin,
+  `content` text CHARACTER SET utf8 COLLATE utf8_bin,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+--
+-- Dumping data for table `data`
+--
+
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user table`
---
--- Creation: Jul 16, 2013 at 08:49 PM
+-- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user table` (
-  `username` varchar(10) NOT NULL,
-  `password` varchar(10) NOT NULL,
-  `email` varchar(50) DEFAULT NULL,
+CREATE TABLE IF NOT EXISTS `user` (
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `userid` tinyint(4) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`userid`)
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `username` (`username`,`password`,`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `user`
+--
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
