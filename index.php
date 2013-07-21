8f615ee61c7c353b96b25aba2e0d76f01686515b
Anurag Upadhaya <anurag@eanurag.com>
2013-07-21 21:40:03 +0530

    Added index.php and style.css
    Template for index page and styling created.

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title> HomePage of CMS ! </title>
+		<link rel="stylesheet"	type="text/css"	href="template/style.css"/>
+	</head>
+	<body>
+		<div id="container">
+			<div id="logo">
+				<h1><a href="index.php">CMS</a></h1>
+				A minimal Content Management System
+			</div>
+			<div id="content">
+				<ol>
+					<li>
+						<!-- Dummy text -->
+						Article Title
+						-	Posted Date
+					</li>
+				</ol>
+
+			</div>
+		</div>
+	</body>
+</html>
