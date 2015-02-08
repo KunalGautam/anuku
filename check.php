9c2dc2e4bce7eb5e1b0aeeb465a79d8040346e1c
Kunal Gautam <kunal@ikunal.in>
2015-02-08 15:18:57 +0530

    Modifying check.php
    used exit() function instead of echo() for better output. Removed unused
    spacing of lines.

--- check.php
+++ check.php
@@ -14,30 +14,16 @@ if (file_exists($filename))
 // This block of function check if details provided are correct or not, if any of the detail is not correct, it will generate mysql error, which will ultimately redirect to the install folder.
 function dbconnect($server, $user, $password, $db)
 {
-	
-	
-	
 	$dsn = 'mysql:dbname='.$db.';host='.$server;
-try {
-    $db = new PDO($dsn, $user, $password);
-	$db = null; // Closing DB
-    
-    }
-catch(PDOException $e)
-    {
-    echo "<h1>Error establishing database connection.</h1>";
-    
-    }
-	
-	
-	
-	
-	
-	
-	
-				
+	try {
+		$db = new PDO($dsn, $user, $password);
+		$db = null; // Closing DB
+		
+		}
+	catch(PDOException $e)
+		{
+		exit("<h1>Error establishing database connection.</h1>");
+		}			
 }
 //End of DB connection checking block.
-dbconnect($server, $user, $password, $db);
-
 ?>
