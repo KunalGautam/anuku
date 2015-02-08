edc3f6047e48f3bc515f6c3f85805e258d84695a
Kunal Gautam <kunal@ikunal.in>
2015-02-08 15:14:57 +0530

    PHP PDO Implementation
    converting check.php to use php PDO class.

--- check.php
+++ check.php
@@ -11,43 +11,33 @@ if (file_exists($filename))
 				exit;
 }
 // End of config file checking code
-// This block or set of if condition if that file contain the variables or not, if it is not defined, it will assign those variables as blank, so that it goes to next function
-if (!isset($server))
-{
-				$server = "";
-}
-if (!isset($user))
-{
-				$user = "";
-}
-if (!isset($password))
-{
-				$password = "";
-}
-if (!isset($db))
-{
-				$db = "";
-}
-//End of if Block for assigning variables if not available in config file
 // This block of function check if details provided are correct or not, if any of the detail is not correct, it will generate mysql error, which will ultimately redirect to the install folder.
 function dbconnect($server, $user, $password, $db)
 {
-				$conn = @mysql_pconnect($server, $user, $password); // "@" is necessary.
-				if (!$conn)
-				{
-								header("Location: install/");
-								exit;
-				} else
-				{
-								/*though this is not needed, it is just placeholder if something pop up in mind of developer :P */
-				}
-				if (!mysql_select_db($db, $conn))
-				{
-								header("Location: install/");
-								exit;
-				}
+	
+	
+	
+	$dsn = 'mysql:dbname='.$db.';host='.$server;
+try {
+    $db = new PDO($dsn, $user, $password);
+	$db = null; // Closing DB
+    
+    }
+catch(PDOException $e)
+    {
+    echo "<h1>Error establishing database connection.</h1>";
+    
+    }
+	
+	
+	
+	
+	
+	
+	
+				
 }
 //End of DB connection checking block.
-
+dbconnect($server, $user, $password, $db);
 
 ?>
